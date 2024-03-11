56108f73ca3b46c267986b5e5e0daab6f420dfab
blelump <blelump@martex.nom.pl>
2024-03-11 16:55:25 +0100

    fix: expose d.ts file

---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+export function from(bundleWithDeps: any, presentation: any, conditionals?: any): Promise<{
+    form: any;
+    i18n: any;
+}>;
+export type SAID = string;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import { OCABox } from "oca.js"
 
 const DEFAULT_LANG = "eng"
